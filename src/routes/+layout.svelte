77ee52e6462af1b0ad4185c1a95e8bde01a5d148
Mario Ruiz <marioalejandroruizsarmiento@gmail.com>
2024-02-20 23:31:51 -0500

    Navigation test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script>
 	import '../app.pcss';
 	import { ModeWatcher } from 'mode-watcher';
+	import Toggle from '$lib/components/Toggle.svelte';
 </script>
 
 <ModeWatcher />
-<slot />
+<article class="prose prose-sm dark:prose-invert container max-w-none p-10 py-10 sm:max-w-5xl">
+	<div class="flex justify-between">
+		<h1>Revision de carpeta</h1>
+		<Toggle></Toggle>
+	</div>
+	<div class="flex justify-end gap-4">
+		<a href="Vistas">Vistas</a>
+		<a href="Reglas">Reglas</a>
+		<a href="Pensum">Pensum</a>
+		<a href="Lista">Lista</a>
+	</div>
+	<slot />
+</article>
---
+++ src/routes/Lista/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import * as Table from '$lib/components/ui/table';
+	import { courseList } from '$lib/courseList.js';
+</script>
+
+<h2>Lista de clases</h2>
+<Table.Root class="not-prose">
+	<Table.Caption>Lista de tus materias vists.</Table.Caption>
+	<Table.Header>
+		<Table.Row>
+			<Table.Head class="w-[100px]">Nombre</Table.Head>
+			<Table.Head>Codigo</Table.Head>
+			<Table.Head>Creditos</Table.Head>
+			<Table.Head class="text-right">Atributos</Table.Head>
+		</Table.Row>
+	</Table.Header>
+	<Table.Body>
+		{#each courseList as course, i (i)}
+			<Table.Row>
+				<Table.Cell class="font-medium">{course.nombre}</Table.Cell>
+				<Table.Cell>{course.codigo}</Table.Cell>
+				<Table.Cell>{course.creditos}</Table.Cell>
+				<Table.Cell class="text-right">{course.atributos}</Table.Cell>
+			</Table.Row>
+		{/each}
+	</Table.Body>
+</Table.Root>
